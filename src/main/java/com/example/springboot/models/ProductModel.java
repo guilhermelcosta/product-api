6f274824d4795e10faf449947bf6555bf895aa29
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-07-01 16:46:24 -0300

    feat: adicionado repositorio com CRUD finalizado

---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,45 @@
+package com.example.springboot.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = ProductModel.TABLE_NAME)
+public class ProductModel implements Serializable {
+
+    public static final String TABLE_NAME = "TB_PRODUCTS";
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private BigDecimal value;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
